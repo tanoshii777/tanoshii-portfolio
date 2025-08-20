e5c965285bf9949b3aa1c8af1c9c921bc4ec709b
Aljoy Remonde Bascon <your_email@example.com>
2025-08-20 09:09:49 +0800

    Change: Layout Description and adding Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
 import Footer from "../components/Home/Footer/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const font = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -13,7 +14,7 @@ const font = Inter({
 
 export const metadata: Metadata = {
   title: "Portfolio | Aljoy",
-  description: "Personal website portfolio of Tanoshii",
+  description: "Welcome to Personal Website Portfolio of Tanoshii",
 };
 
 export default function RootLayout({
@@ -26,6 +27,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
